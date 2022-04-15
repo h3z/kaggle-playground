5fc30f1571ba7e231f1674d6b88fb3b079e4c5ce
Your Name <you@example.com>
2022-04-15 09:27:00 +0800

    .

---
+++ src/base_experiment.py
@@ -0,0 +1,28 @@
+import wandb
+from dataset_util import Dataset
+
+
+class Experiment:
+    def __init__(self, ds: Dataset, params) -> None:
+        self.ds = ds
+        self.params = params
+        print(params)
+        wandb.run.name = self.name
+        wandb.run.notes = self.description
+
+    def train(self):
+        pass
+
+    def predict(self):
+        pass
+
+    # def report_final_result(self):
+    #     pass
+
+    @property
+    def name(self):
+        pass
+
+    @property
+    def description(self):
+        pass
